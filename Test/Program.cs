01a5afed5652588dae8a64d2b594d3c6e7d95c1d
Ibraheem Khazbak <ibraheemmkhazbak@gmail.com>
2023-08-10 15:20:08 +0300

    BugFix: Numbers aren't displayed correctly

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,11 +1,16 @@
 ﻿using num2a_C_;
+using System;
 
 INum2a svc = new Num2a();
 
-List<string> output = new List<string>();
+List<(string, long)> output = new List<(string, long)>();
+Random r = new Random();
+long num;
 for (int i = 0; i <= 100; i++)
 {
-    output.Add(svc.ProcessNumW(i, "دولار", "دولارا", "دولارين", "دولارات", false, false));
+    num = r.NextInt64(0, 1000000);
+    output.Add((svc.ProcessNumW(i, "دولار", "دولارا", "دولارين", "دولارات", false, false), i));
 }
+
 Console.WriteLine("done");
 Console.ReadLine();
